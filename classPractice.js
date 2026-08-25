258cf74b53696d0c8ae5652e0372dc35527ba02c
Akbar <akbarn5432@gmail.com>
2026-08-25 15:06:01 +0800

    Add Object and Item

--- classPractice.js
+++ classPractice.js
@@ -12,3 +12,11 @@ class Product{
     }
 }
 
+const productInventory = new Map()
+const product1 = new Product('ASTAT001','FABERCASTELL','Highlight-101',5.60)
+const product2 = new Product('ASTAT002','LUNA','Highlight-102',5.60)
+const product3 = new Product('ASTAT003','STABILO','Highlight-103',5.60)
+const product4 = new Product('ASTAT004','BUNCHO','Highlight-104',5.60)
+const product5 = new Product('ASTAT005','STEADTLER','Highlight-105',5.60)
+
+
