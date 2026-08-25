6e482d90412411f87fba3b24a8f98638f06e7e10
Akbar <akbarn5432@gmail.com>
2026-08-25 14:58:05 +0800

    First commit

---
+++ classPractice.js
@@ -0,0 +1,9 @@
+
+class Product{
+    constructor(id,brand,productName,price,){
+        this.name = id;
+        this.brand = brand
+        this.name = productName
+        this.name = price
+    }
+}
