2e3e538e4d587b6bc3b96826adf3ec8841f60ac9
Akbar <akbarn5432@gmail.com>
2026-08-26 15:47:56 +0800

    Re-design

--- classPractice.js
+++ classPractice.js
@@ -1,22 +1,7 @@
-
-class Product{
-    constructor(id,brand,productName,price,){
+class Menu{
+    constructor(id,dishes,price){
         this.id = id;
-        this.brand = brand
-        this.productName = productName
+        this.dishes = dishes
         this.price = price
     }
-
-    getProductDetail(){
-        return `product : ${this.productName} brand: ${this.brand} price: ${this.price}`
-    }
 }
-
-const productInventory = new Map()
-const product1 = new Product('ASTAT001','FABERCASTELL','Highlight-101',5.60)
-const product2 = new Product('ASTAT002','LUNA','Highlight-102',5.60)
-const product3 = new Product('ASTAT003','STABILO','Highlight-103',5.60)
-const product4 = new Product('ASTAT004','BUNCHO','Highlight-104',5.60)
-const product5 = new Product('ASTAT005','STEADTLER','Highlight-105',5.60)
-
-
