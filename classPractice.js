8b5fd3fefb89c6d141740fe6d9d495e1899d5a2f
Akbar <akbarn5432@gmail.com>
2026-08-26 16:27:23 +0800

    Re-design 3

--- classPractice.js
+++ classPractice.js
@@ -11,3 +11,17 @@ class Menu{
     }
 }
 
+const order = new Map()
+
+const dish1 =  new Menu('0001','Chicken Parmigiana',15.00)
+const dish2 =  new Menu('0002','Chicken chop',15.00)
+const dish3 =  new Menu('0003','Chicken Maryland',15.00)
+
+
+order.set(dish1.id,dish1)
+order.set(dish2.id,dish2)
+order.set(dish3.id,dish3)
+
+const display = order.get("0002")
+
+console.log(display.getDetail())
