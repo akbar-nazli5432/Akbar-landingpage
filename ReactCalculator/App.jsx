63f95d9a6da920e9c151d251c426d4071e40407c
Akbar <akbarn5432@gmail.com>
2026-09-16 00:51:39 +0800

    Add input number function

--- ReactCalculator/App.jsx
+++ ReactCalculator/App.jsx
@@ -1,1 +1,22 @@
 //Project-React-Calculator
+
+import { useState } from "react";
+import "./App.css";
+
+export default function App() {
+  const [display, setDisplay] = useState("0");
+  const [firstNumber, setFirstNumber] = useState(null);
+  const [operator, setOperator] = useState(null);
+  const [waitingForSecondNumber, setWaitingForSecondNumber] = useState(false);
+
+  // Number button
+  const inputNumber = (number) => {
+    if (waitingForSecondNumber) {
+      setDisplay(number);
+      setWaitingForSecondNumber(false);
+      return;
+    }
+
+    setDisplay(display === "0" ? number : display + number);
+  };
+
